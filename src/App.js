ae7d42fbf727fba1c639f58851e5d91681591b4f
FIMehedi <fokrulislammehedi2000@gmail.com>
2020-03-12 15:50:31 +0600

    props done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const friendStyle = {
+    textAlign : 'left'
+  };
+
+  const data = [
+    {name: 'Fokrul Islam Mehedi', age: 17, gender: 'Male'},
+    {name: 'Afsar Uddin', age: 18, gender: 'Male'},
+    {name: 'Emon Hasan', age: 20, gender: 'Male'},
+    {name: 'Hasib Uddin', age: 19, gender: 'Male'}
+  ];
+
+  const friends = ["Fokrul Islam", "Afsar", "Emon", "Nyeem", "Somir"]; 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {
+          data.map(person => <Info person={person}></Info>)
+        }
+    
+        
+
       </header>
     </div>
   );
+}
+
+function Info(props) {
+  const InfoStyle = {
+    border: '1px solid white',
+    margin: '10px',
+    padding: '10px 30px',
+    width: '60%',
+  }
+  const {name, age, gender} = props.person;
+  return(
+    <div style={InfoStyle}>
+      <h3>Name : {name}</h3>
+      <h5>Age : {age}</h5>
+      <h5>Gender: {gender}</h5>
+    </div>
+  )
 }
 
 export default App;
